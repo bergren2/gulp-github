e74fb43d26feaf8b88e54385d527dead1a738f2f
Dan Bergren <dbergren@frontlineed.com>
2016-11-04 16:51:10 -0500

    fix file name, init task template

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/* jslint node: true */
+var gulp = require("gulp");
+var config = require("./config.json");
+
+gulp.task("default", ["authenticate"]);
+
+gulp.task("authenticate", function () {
+  // TODO
+});
