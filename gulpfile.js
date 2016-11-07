d48296f580b3e032548695a802033744726ef16e
Dan Bergren <dbergren@frontlineed.com>
2016-11-07 16:42:19 -0600

    add GitHub to Gulp WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 /* jslint node: true */
 var gulp = require("gulp");
+var GitHubApi = require("github");
 var config = require("./config.json");
+
+var github = new GitHubApi({
+  debug: false
+});
 
 gulp.task("default", ["authenticate"]);
 
 gulp.task("authenticate", function () {
-  // TODO
+  return github.authenticate({
+    type: "oauth",
+    token: config.token,
+  });
+});
+
+gulp.task("pulls", ["authenticate"], function () {
+  return github.issues.getForUser({
+    username: "bergren2"
+  }, function (err, res) {
+    if (err) {
+      console.log(err.toJSON());
+    } else {
+      console.log(res);
+    }
+  });
 });
